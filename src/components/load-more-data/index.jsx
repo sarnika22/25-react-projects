f6f8c6c51fb6cd5f4af31d7fb14e1e2e4a0862f4
Sarnika K <sharu022kemp@gmail.com>
2024-04-18 20:20:12 +0530

    count bug

---
+++ src/components/load-more-data/index.jsx
@@ -0,0 +1,10 @@
+
+
+//infinite cloning
+export default function LoadMore(){
+    return <div className="container">
+    const [loadingState, setloading] =  useState(false);
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0); {/*count how many times clicking */}
+    </div>
+}
