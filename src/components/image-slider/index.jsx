d25f5fef2ec6736714773d5e0880b48eecd1f1dc
Sarnika K <sharu022kemp@gmail.com>
2024-04-18 07:46:27 +0530

    1.14.38 bug

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -2,11 +2,14 @@
 /* eslint-disable no-unused-vars */
 import { useEffect } from "react";
 import { useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } 
+from "react-icons/bs";
+import "./style.css";
 
 export default function ImageSlider({ url, limit = 5, page = 1 }) {
   const [Images, setImages] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(0);
-  const [errorMsg, setErrorMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState(null);
   const [loading, setLoading] = useState(false);
 
   async function fetchImages(getUrl) {
@@ -20,6 +23,7 @@ export default function ImageSlider({ url, limit = 5, page = 1 }) {
         setLoading(false);
       }
     } catch (e) {
+      console.log(e);
       setErrorMsg(e.message);
       setLoading(false);
     }
@@ -33,5 +37,33 @@ export default function ImageSlider({ url, limit = 5, page = 1 }) {
   if (errorMsg !== null) {
     return <div>Error Occured! {errorMsg}</div>;
   }
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      {Images && Images.length
+        ? Images.map((imageItem) => (
+            <img
+              src={imageItem.download_url}
+              alt={imageItem.download_url}
+              key={imageItem.id}
+              className="current-image"
+            />
+          ))
+        : null}
+        <BsArrowRightCircleFill
+        className="arrow arrow-right"/>
+        <span className="cirlce-indicators">
+          {
+            Images && Images.length ? 
+            Images.map((_,index) => <button
+            key = {index}
+            className="current-indicator">
+            
+            </button>
+            )
+            : null
+          }
+        </span>
+    </div>
+  );
 }
