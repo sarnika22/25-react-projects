0304e8ef62058325d06b048888315a8e79a0a427
Sarnika K <sharu022kemp@gmail.com>
2024-04-18 20:58:17 +0530

    misplaced div, didn't  import usestate properly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import ImageSlider from './components/image-slider';
+//import ImageSlider from './components/image-slider';
+import LoadMore from './components/load-more-data';
 //import StarRating from './components/star-rating';
 //import Accordion from './components/accordion';
 //import RandomColor from './components/random-color';
@@ -14,10 +15,12 @@ function App() {
       {/* star rating component */}
       {/*<StarRating noOfStars={10}/>*/}
       {/* image slider component */}
-      <ImageSlider 
+      {/*<ImageSlider 
       url={"https://picsum.photos/v2/list"}
       page={'1'}
-      limit={'4'}/>
+      limit={'4'}/>*/}
+      {/* load more data component */}
+      <LoadMore/>
     </div>
   );
 }
--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -1,20 +1,27 @@
+/* eslint-disable no-lone-blocks */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unreachable */
+
+import { useState } from "react";
 
 
 //infinite cloning
 export default function LoadMore(){
-    return <div className="container">
+    
     const [loadingState, setloading] =  useState(false);
     const [products, setProducts] = useState([]);
     const [count, setCount] = useState(0); {/*count how many times clicking */}
-    </div>
+    
     
     async function fetchProducts(){
         try {
             
-            // eslint-disable-next-line no-unused-vars
-            const response = await fetch( `https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`);
-            
+        
+            const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`);
+
+            const result = await response.json();
+            console.log(result);
         } catch (e) {
             console.log(e.message);
         }
