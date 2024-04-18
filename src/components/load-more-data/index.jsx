ee1216f738dc7d447594e9ef32b49ddf4762d073
Sarnika K <sharu022kemp@gmail.com>
2024-04-18 22:33:30 +0530

    load more data done

--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -3,32 +3,79 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unreachable */
 
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import "./styles.css";
 
 //infinite cloning
-export default function LoadMore(){
-    
-    const [loadingState, setloading] =  useState(false);
-    const [products, setProducts] = useState([]);
-    const [count, setCount] = useState(0); {/*count how many times clicking */}
-    
-    
-    async function fetchProducts(){
-        try {
-            
-        
-            const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`);
+export default function LoadMore() {
+  const [loading, setloading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
+  {
+    /*count how many times clicking */
+  }
 
-            const result = await response.json();
-            console.log(result);
-        } catch (e) {
-            console.log(e.message);
-        }
+  async function fetchProducts() {
+    try {
+      setloading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+
+      const result = await response.json();
+      console.log(result);
+
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setloading(false);
+      }
+    } catch (e) {
+      console.log(e.message);
+      setloading(false);
     }
+  }
 
-    // eslint-disable-next-line no-undef
-    useEffect(() => {
-        fetchProducts()
-    },[])
+  // eslint-disable-next-line no-undef
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
+
+  useEffect(() => {
+    if (products.length >= 100) {
+      setDisableButton(true);
+    } else {
+      setDisableButton(false);
+    }
+  }, [products]);
+
+  if (loading) {
+    return <div>Loading data! Please wait</div>;
+  }
+
+  return (
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="product" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+              </div>
+            ))
+          : null}
+      </div>
+      <div>
+        <button
+          disabled={disableButton}
+          className="button-container"
+          onClick={() => setCount(count + 1)}
+        >
+          Load more products
+        </button>
+        {disableButton ? <p>No more products to load</p> : null}
+      </div>
+    </div>
+  );
 }
