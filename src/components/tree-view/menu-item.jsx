0bca2513e34d654ca2c599e7ca678268078487ad
Sarnika K <sharu022kemp@gmail.com>
2024-04-19 08:04:25 +0530

    tree view done

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import MenuList from "./menu-list";
+import {FaMinus, FaPlus} from "react-icons/fa"
 
 
 export default function MenuItem({item}){
@@ -16,19 +17,24 @@ export default function MenuItem({item}){
     
     console.log(displayCurrentChildren);
 
-    return <li>
-        <div style={{display: "flex", gap:'20px'}}>
+    return (<li >
+        <div className="menu-item">
         <p>{item.label}</p>
 
         {
-            item && item.children && item.children.length  ? <span onClick={() => handleToggleChildren()}> + </span> : null
+            item && item.children && item.children.length  ? <span onClick={() => handleToggleChildren(item.label)}> 
+                {
+                    displayCurrentChildren[item.label] ? < FaMinus color="white" size={25}/> : <FaPlus color="white" size={25}/>
+                }
+             </span> : null
         }
         </div>
 
         {
-            item && item.children && item.children.length > 0
+            item && item.children && item.children.length > 0 && displayCurrentChildren[item.label]
             ? <MenuList list = {item.children}/>
              : null
         }
     </li>
+    )
 }                                   
