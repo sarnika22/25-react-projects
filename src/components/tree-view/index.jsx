bfc034220292b25b0a07a45735ac125361cdb371
Sarnika K <sharu022kemp@gmail.com>
2024-04-19 09:09:13 +0530

    2.26.11

--- src/components/tree-view/index.jsx
+++ src/components/tree-view/index.jsx
@@ -1,7 +1,7 @@
 import MenuList from "./menu-list";
-import "./styles.css"; 
+import "./styles.css";
 
-export default function TreeView({menus = [] }) {
+export default function TreeView({ menus = [] }) {
   return (
     <div className="tree-view-container">
       <MenuList list={menus} />
--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,40 +1,42 @@
 import { useState } from "react";
 import MenuList from "./menu-list";
-import {FaMinus, FaPlus} from "react-icons/fa"
+import { FaMinus, FaPlus } from "react-icons/fa";
 
+export default function MenuItem({ item }) {
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
 
-export default function MenuItem({item}){
+  function handleToggleChildren(getCurrentLabel) {
+    console.log(getCurrentLabel);
+    setDisplayCurrentChildren({
+      ...displayCurrentChildren,
+      [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+    });
+  }
 
-    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+  console.log(displayCurrentChildren);
 
-    function handleToggleChildren(getCurrentLabel){
-        console.log(getCurrentLabel);
-        setDisplayCurrentChildren({
-            ...displayCurrentChildren,
-            [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
-        });
-    }
-    
-    console.log(displayCurrentChildren);
-
-    return (<li >
-        <div className="menu-item">
+  return (
+    <li>
+      <div className="menu-item">
         <p>{item.label}</p>
 
-        {
-            item && item.children && item.children.length  ? <span onClick={() => handleToggleChildren(item.label)}> 
-                {
-                    displayCurrentChildren[item.label] ? < FaMinus color="white" size={25}/> : <FaPlus color="white" size={25}/>
-                }
-             </span> : null
-        }
-        </div>
+        {item && item.children && item.children.length ? (
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {displayCurrentChildren[item.label] ? (
+              <FaMinus color="white" size={25} />
+            ) : (
+              <FaPlus color="white" size={25} />
+            )}
+          </span>
+        ) : null}
+      </div>
 
-        {
-            item && item.children && item.children.length > 0 && displayCurrentChildren[item.label]
-            ? <MenuList list = {item.children}/>
-             : null
-        }
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayCurrentChildren[item.label] ? (
+        <MenuList list={item.children} />
+      ) : null}
     </li>
-    )
-}                                   
+  );
+}
