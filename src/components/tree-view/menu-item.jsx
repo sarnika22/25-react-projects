3c50ec604cec81f9bb3b6bc9bea30a2c51365d81
Sarnika K <sharu022kemp@gmail.com>
2024-04-19 07:39:04 +0530

    onclick bug & menu item cleared 1.53.44

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import MenuList from "./menu-list";
 
 
-export default function MenuItem(item){
-    <div>
-        Menu Item
-    </div>
-}
+export default function MenuItem({item}){
+
+    const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
+
+    function handleToggleChildren(getCurrentLabel){
+        console.log(getCurrentLabel);
+        setDisplayCurrentChildren({
+            ...displayCurrentChildren,
+            [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+        });
+    }
+    
+    console.log(displayCurrentChildren);
+
+    return <li>
+        <div style={{display: "flex", gap:'20px'}}>
+        <p>{item.label}</p>
+
+        {
+            item && item.children && item.children.length  ? <span onClick={() => handleToggleChildren()}> + </span> : null
+        }
+        </div>
+
+        {
+            item && item.children && item.children.length > 0
+            ? <MenuList list = {item.children}/>
+             : null
+        }
+    </li>
+}                                   
--- src/components/tree-view/menu-list.jsx
+++ src/components/tree-view/menu-list.jsx
@@ -2,10 +2,10 @@ import MenuItem from "./menu-item";
 
 export default function MenuList({ list = [] }) {
   return (
-    <div className="menu-list-container">
+    <ul className="menu-list-container">
       {list && list.length
         ? list.map((listItem) => <MenuItem item={listItem} />)
         : null}
-    </div>
+    </ul>
   );
 }
