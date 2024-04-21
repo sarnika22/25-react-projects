db684b0e82dd2c8019fce522c2bff8b005e98a65
Sarnika K <sharu022kemp@gmail.com>
2024-04-21 12:21:55 +0530

    custom modal done

--- src/components/custom-modal-popup/modal-test.jsx
+++ src/components/custom-modal-popup/modal-test.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 import Modal from "./modal";
-
+import "./modal.css";
 
 export default function ModalTest() {
+  const [showModalPopup, setShowModalPopup] = useState(false);
 
-    const [showModalPopup, setShowModalPopup] = useState(false);
+  function handleToggleModalPopup() {
+    setShowModalPopup(!showModalPopup);
+  }
 
-    function handleToggleModalPopup() {
-        setShowModalPopup(!showModalPopup);
-    }
-    
-    return <div>
-        <button onClick={handleToggleModalPopup}>Open Modal PopUp!</button>
-        {
-            showModalPopup  && <Modal/>
-        }
+  function onClose() {
+    setShowModalPopup(false);
+  }
+
+  return (
+    <div>
+      <button onClick={handleToggleModalPopup}>Open Modal PopUp!</button>
+      {showModalPopup && (
+        <Modal id={"custom-id"}
+          header={<h1>"Customized Header"</h1>}
+          footer={<h1>"Customized Footer"</h1>}
+          onClose={onClose}
+          body={<div>Customized body</div>}
+        />
+      )}
     </div>
+  );
 }
