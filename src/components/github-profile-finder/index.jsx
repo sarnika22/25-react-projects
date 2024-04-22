26f91be4eaed1949ba331f56dba299d1776c4999
Sarnika K <sharu022kemp@gmail.com>
2024-04-22 16:48:43 +0530

    git profile 3.29.10

--- src/components/github-profile-finder/index.jsx
+++ src/components/github-profile-finder/index.jsx
@@ -1,20 +1,37 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import User from "./user";
 
 export default function GithubProfileFinder() {
   const [username, setUsername] = useState("sarnika22");
+  const [userData, setUserdata] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-async function fetchGithubUserData() {
+  async function fetchGithubUserData() {
+    setLoading(true);
     const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
+    if (data) {
+      setUserdata(data);
+      setLoading(false);
+      setUsername("");
+    }
     console.log(data);
-}
+  }
 
-  function handleSubmit() {}
+  function handleSubmit()
+  {
+    fetchGithubUserData()
+  }
 
   useEffect(() => {
-    fetchGithubUserData()    
-  },[])
+    fetchGithubUserData();
+  }, []);
+
+  if (loading) {
+    return <h1>Loading... Please Wait</h1>;
+  }
+
   return (
     <div className="github-profile-container">
       <div className="input-wrapper">
@@ -27,6 +44,7 @@ async function fetchGithubUserData() {
         />
         <button onClick={handleSubmit}> onSearch</button>
       </div>
+      { userData !== null ? <User user={userData} /> : null}
     </div>
   );
 }
