1c4eb618e517135e1724c7d3cfac017c7b57e436
Sarnika K <sharu022kemp@gmail.com>
2024-04-23 22:19:14 +0530

    tictactoe done 4.16.42

--- src/components/tictactoe/index.jsx
+++ src/components/tictactoe/index.jsx
@@ -1,62 +1,83 @@
-import { useState } from 'react';
-import './style.css';
-function Square({value, onClick}) {
-    return <button onClick={onClick} className="square">{value}</button>
+import { useEffect, useState } from "react";
+import "./style.css";
+function Square({ value, onClick }) {
+  return (
+    <button onClick={onClick} className="square">
+      {value}
+    </button>
+  );
 }
 
 export default function TicTacToe() {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  const [xIsNext, setXIsNext] = useState(true);
+  const [status, setStatus] = useState("");
 
-    const [squares, setSquares] = useState(Array(9).fill(''));
-    const [xIsNext, setXIsNext] = useState(true);
-
-    function calculateWinner(squares) {
-        const winningPatterns = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
-        ]
-       for(let i=0; i<winningPatterns.length; i++) {
-         const [a, b, c] = winningPatterns[i];
-         if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
-          {
-           return squares[a];
-         }
-         return null;
+  function calculateWinner(squares) {
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [x, y, z] = winningPatterns[i];
+      if (
+        squares[x] &&
+        squares[x] === squares[y] &&
+        squares[x] === squares[z]
+      ) {
+        return squares[x];
+      }
     }
 
-    function handleClick(getCurrentSquare) {
-        let cpySquares = [...squares]; 
-        if(cpySquares[getCurrentSquare]) return;
-        cpySquares[getCurrentSquare] = xIsNext ? 'X' : 'O';
-        setXIsNext(!xIsNext);
-        setSquares(cpySquares);
-    }
-    
+    return null;
+  }
 
+  function handleClick(getCurrentSquare) {
+    let cpySquares = [...squares];
+    if (calculateWinner(cpySquares) || cpySquares[getCurrentSquare]) return;
+    cpySquares[getCurrentSquare] = xIsNext ? "X" : "O";
+    setXIsNext(!xIsNext);
+    setSquares(cpySquares);
+  }
+
+  function handleRestart() {
+    setXIsNext(true);
+    setSquares(Array(9).fill(""));
+  }
+
+  useEffect(() => {
+    if (!calculateWinner(squares) && squares.every((item) => item !== ""))
+      setStatus("This is Draw! please restart");
+    else if (calculateWinner(squares))
+      setStatus(`Winner: ${calculateWinner(squares)} Please restart the game!`);
+    else setStatus(`Next player is: ${xIsNext ? "X" : "O"}`);
+  }, [squares, xIsNext]);
 
   return (
     <div className="tic-tac-toe-container">
       <div className="row">
-        <Square value={squares[0]} onClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onClick={() => handleClick(2)}/>
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="row">
-        <Square value={squares[3]} onClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onClick={() => handleClick(5)}/>
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="row">
-        <Square value={squares[6]} onClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onClick={() => handleClick(8)}/>
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
+      <h1>{status}</h1>
+      <button onClick={handleRestart}>Restart</button>
     </div>
   );
-}
 }
